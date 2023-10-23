fd78a964ea2d1b568f0df28d518db4aaec1d4c43
Yonatan Rubin <yooonatan@gmail.com>
2023-10-23 15:50:29 +0300

    allow custom language and simplify new proof sources generation

--- proof-verification/src/logic_verify_file.py
+++ proof-verification/src/logic_verify_file.py
@@ -1,5 +1,9 @@
+import pathlib
+from abc import abstractmethod
+from itertools import zip_longest
 from openpyxl import *
 from openpyxl.styles import PatternFill
+from docx import *
 import logic_rules as proof
 from tkinter.filedialog import askopenfilename
 from shutil import which
@@ -9,11 +13,6 @@ import os
 import re
 
 langmap = configparser.ConfigParser()
-
-ERROR = PatternFill(start_color="2d0c45", end_color="2d0c45", fill_type="solid")
-RED = PatternFill(start_color="00FF0000", end_color="00FF0000", fill_type="solid")
-GREEN = PatternFill(start_color="0000FF00", end_color="0000FF00", fill_type="solid")
-YELLOW = PatternFill(start_color="FFFF0000", end_color="FFFF0000", fill_type="solid")
 
 
 def always_valid(*args, **kwargs):
@@ -43,6 +42,7 @@ reason_map = {
     "DN": proof.double_negation,
     "REP": proof.rep,
     "⨳I": proof.x_in,
+    "⨳O": always_valid,
     "∀O": proof.all_out,
     "~∀O": proof.not_all_out,
     "∃O": proof.exist_out,
@@ -74,13 +74,9 @@ def extract_show_follow(tp):
     return show_map[tp.upper()]
 
 
-def find_start(column):
-    for index, cell in enumerate(column):
-        if cell.value is not None:
-            return index + 1
-
-
 def clean_language(line):
+    if not line:
+        return line
     if "symbols" not in langmap:
         return line
     for operand, symbol in langmap["symbols"].items():
@@ -95,78 +91,186 @@ def clean_line(line):
     return line.replace("[", "(").replace("]", ")").replace(" ", "").rstrip()
 
 
-def handle_showline(sheet, index):
-    tp = clean_language(sheet.cell(index, 3).value)
-    if tp is None or tp == "":
-        sheet.cell(index, 3).fill = YELLOW
-        return
-    p = re.compile("show:?", re.IGNORECASE)
-    show = proof.shunting_yard_statement(p.sub("", clean_line(sheet.cell(index, 2).value)))
-    following_size, rule = extract_show_follow(tp)
-    following = [p.sub("", clean_line(sheet.cell(index + line + 1, 2).value)) for line in range(following_size)]
-    following = list(map(proof.shunting_yard_statement, following))
-
-    if len(following) <= 1:
-        valid = rule(show, *following)
-    else:
-        valid = rule(show, following)
-
-    if valid is None:
-        return
-    elif not valid:
-        for line in range(following_size + 1):
-            sheet.cell(index + line, 3).fill = RED
-    else:
-        for line in range(following_size + 1):
-            sheet.cell(index + line, 3).fill = GREEN
+def find_start(column):
+    for index, cell in enumerate(column):
+        if cell.value is not None:
+            return index + 1
 
 
-def handle_proofline(sheet, index, proof_index):
-    output = clean_line(sheet.cell(index, 2).value)
-    reason = sheet[f"C{index}"].value
-    if reason is None or reason == "":
-        sheet.cell(index, 3).fill = YELLOW
-        return
-    relies, rule = extract_reason(reason)
-    indices = [index - (proof_index - i) for i in relies]
-    relies = list(
-        map(
-            proof.shunting_yard_statement,
-            map(lambda cell: clean_line(sheet.cell(cell, 2).value), indices),
-        )
-    )
-    if len(relies) <= 1:
-        valid = rule(*relies, proof.shunting_yard_statement(output))
-    else:
-        valid = rule(relies, proof.shunting_yard_statement(output))
-    if valid is None:
-        return
-    elif not valid:
-        sheet.cell(index, 3).fill = RED
-    else:
-        sheet.cell(index, 3).fill = GREEN
+class Proof:
+    def __init__(self):
+        self._index = 1
+        self.show_regex = re.compile("show:?", re.IGNORECASE)
+
+    def handle_proof(self):
+        while self.get_line(self._index)[0]:
+            row = self.get_line(self._index)
+            if not isinstance(row[0], int) or row[1] is None:
+                self._index += 1
+                continue
+            try:
+                if "SHOW" in row[1].upper():
+                    self.verify_showline()
+                else:
+                    self.verify_proofline()
+            except:
+                self.mark_error()
+            self._index += 1
+
+    def verify_showline(self):
+        _, line, method = self.get_line(self._index)
+        line = self.show_regex.sub("", line)
+        if method is None or method == "":
+            self.mark_unknown()
+            return
+        show = proof.shunting_yard_statement(line)
+        following_size, rule = extract_show_follow(method)
+        following = [self.get_line(self._index + l + 1)[1] for l in range(following_size)]
+        following = [self.show_regex.sub("", l) for l in following]
+        following = [proof.shunting_yard_statement(l) for l in following]
+
+        if len(following) <= 1:
+            valid = rule(show, *following)
+        else:
+            valid = rule(show, following)
+
+        if valid is None:
+            return
+        elif not valid:
+            for line in range(following_size + 1):
+                self.mark_failure(self._index + line)
+        else:
+            for line in range(following_size + 1):
+                self.mark_success(self._index + line)
+
+    def verify_proofline(self):
+        _, line, reason = self.get_line(self._index)
+        if reason is None or reason == "":
+            self.mark_unknown()
+            return
+        relies, rule = extract_reason(reason)
+        relies = list(map(proof.shunting_yard_statement,
+                          map(lambda row: self.get_line(row)[1], relies)))
+        if len(relies) <= 1:
+            valid = rule(*relies, proof.shunting_yard_statement(line))
+        else:
+            valid = rule(relies, proof.shunting_yard_statement(line))
+        if valid is None:
+            return
+        elif not valid:
+            self.mark_failure()
+        else:
+            self.mark_success()
+
+    def get_line(self, index):
+        line = self._get_raw_line(index)
+        line[1] = clean_line(line[1])
+        line[2] = clean_language(line[2])
+        if isinstance(line[0], str) and line[0].isdigit():
+            line[0] = int(line[0])
+        return line
+
+    @abstractmethod
+    def _get_raw_line(self, index):
+        pass
+
+    @abstractmethod
+    def mark_error(self, index=None):
+        pass
+
+    @abstractmethod
+    def mark_unknown(self, index=None):
+        pass
+
+    @abstractmethod
+    def mark_failure(self, index=None):
+        pass
+
+    @abstractmethod
+    def mark_success(self, index=None):
+        pass
+
+
+class ExcelProof(Proof):
+    ERROR = PatternFill(start_color="2d0c45", end_color="2d0c45", fill_type="solid")
+    RED = PatternFill(start_color="00FF0000", end_color="00FF0000", fill_type="solid")
+    GREEN = PatternFill(start_color="0000FF00", end_color="0000FF00", fill_type="solid")
+    YELLOW = PatternFill(start_color="FFFF0000", end_color="FFFF0000", fill_type="solid")
+
+    def __init__(self, sheet, start=0):
+        self.sheet = sheet
+        self.start = start
+        super().__init__()
+
+    def mark_unknown(self, index=None):
+        index = index or self._index
+        self.sheet.cell(self.start + index, 3).fill = self.YELLOW
+
+    def mark_failure(self, index=None):
+        index = index or self._index
+        self.sheet.cell(self.start + index, 3).fill = self.RED
+
+    def mark_success(self, index=None):
+        index = index or self._index
+        self.sheet.cell(self.start + index, 3).fill = self.GREEN
+
+    def mark_error(self, index=None):
+        index = index or self._index
+        self.sheet.cell(self.start + index, 3).fill = self.ERROR
+
+    def _get_raw_line(self, index):
+        return [self.sheet.cell(self.start + index, i + 1).value for i in range(3)]
+
+
+class TextProof(Proof):
+    def __init__(self, text, marking_format="{0} | {1}"):
+        super().__init__()
+        self.text = text
+        self.line_regex = re.compile(r"(\d*)\.?[.,|\s]*([^0-9.,|]*)[,.|\s]+(.*)?")
+        self.markings = [""] * len(text)
+        self.marking_format = marking_format
+
+    def mark_error(self, index=None):
+        index = index or self._index
+        self.markings[index - 1] = "!ERROR"
+
+    def mark_unknown(self, index=None):
+        index = index or self._index
+        self.markings[index - 1] = "?"
+
+    def mark_failure(self, index=None):
+        index = index or self._index
+        self.markings[index - 1] = "X"
+
+    def mark_success(self, index=None):
+        index = index or self._index
+        self.markings[index - 1] = "V"
+
+    def _get_raw_line(self, index):
+        if index > len(self.text):
+            return [None, None, None]
+        line = self.line_regex.match(self.text[index - 1])
+        if not line:
+            return [None, None, None]
+        return list(line.groups()[:3])
+
+    def get_marked(self):
+        return [self.marking_format.format(a, b) if b else a for a, b in zip(self.text, self.markings)]
+
+
+def find_indices(list_to_check, item_to_find):
+    for idx, value in enumerate(list_to_check):
+        if value == item_to_find:
+            yield idx
 
 
 def verify_excel(file):
     wb = load_workbook(file, data_only=True)
     sheet = wb.active
-    index = find_start(sheet["C"])
-    while sheet[f"A{index}"].value is not None:
-        index += 1
-        try:
-            proof_index = sheet.cell(index, 1).value
-            output = clean_line(sheet.cell(index, 2).value)
-            if not output:
-                continue
-            if not isinstance(proof_index, int):
-                continue
-            if "SHOW" in output.upper():
-                handle_showline(sheet, index)
-            else:
-                handle_proofline(sheet, index, proof_index)
-        except Exception as e:
-            print(e)
-            sheet.cell(index, 3).fill = ERROR
+    indices = find_indices([cell.value for cell in sheet["A"]], 1)
+    for start in indices:
+        proof = ExcelProof(sheet, start)
+        proof.handle_proof()
     name = file.split(".")[0]
     wb.save(f"{name}-checked.xlsx")
 
@@ -196,18 +300,39 @@ def verify_ods(f):
     return True
 
 
+def verify_text(file):
+    name = file.split(".")[0]
+    checked = pathlib.Path(f"{name}-checked.txt")
+    full_file = pathlib.Path(file).read_text().splitlines()
+    start_line_regex = re.compile(r"^\s*1[\s.|]")  # the line starts with the number 1 and seperator
+    indices = list(map(lambda x: x[0], filter(lambda x: start_line_regex.match(x[1]), enumerate(full_file))))
+    ranges = zip_longest(indices, indices[1:])
+    with checked.open("w") as checked:
+        for start, end in ranges:
+            proof = TextProof(full_file[start:end])
+            proof.handle_proof()
+            checked.writelines([p + "\n" for p in proof.get_marked()])
+    return True
+
+
 def verify_file(f):
     if f.endswith(".xlsx"):
         verify_excel(f)
         return True
     if f.endswith(".ods"):
         return verify_ods(f)
+    if f.endswith(".txt"):
+        return verify_text(f)
     return False
 
 
-if __name__ == "__main__":
-    langmap.read_dict(
-        {"symbols": {"or": "||", "not": "!", "if": ">>", "iff": "<>", "all": "@", "some": "#", "contradiction": "XX"}})
+if True and __name__ == "__main__":
+    configs = [os.getcwd(), os.path.join(os.path.expanduser("~"), ".config", "logic"),
+               os.path.dirname(os.path.abspath(__file__)),
+               "/etc", "/usr/local/etc", os.path.join(os.getenv("APPDATA", "", ), "logic")]
+    configs = list(map(lambda x: os.path.join(x, "proof-verification.ini"), configs))
+    configs = list(filter(os.path.exists, configs))
+    langmap.read(configs)
     f = ""
     home_directory = os.path.expanduser("~")
     documents_directory = os.path.join(home_directory, "Documents")
@@ -215,5 +340,5 @@ if __name__ == "__main__":
     while not verify_file(f):
         f = askopenfilename(
             initialdir=documents_directory,
-            filetypes=(("Spreadsheet files", "*.xlsx *.ods"), ("All Files", "*.*")),
+            filetypes=(("Spreadsheet files", "*.xlsx *.ods"), ("Raw Text", "*.txt"), ("All Files", "*.*")),
         )
